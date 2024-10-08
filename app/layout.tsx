2ab5603aa05fd9f821c49423aa021d03fe461d3b
Angel Marenco <jsmarenco.dev@gmail.com>
2024-10-07 19:56:00 -0600

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,8 @@ export default function RootLayout({
             <Footer />
           </div>
         </Providers>
+
+        <script defer src="https://analytics.chesko.dev/script.js" data-website-id="80ac8c92-8649-491e-86f8-d769ecd0af7a"></script>
       </body>
     </html>
   );
